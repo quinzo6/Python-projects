ffe1a0357402e80de84b6a8269c3071f617d7bcb
kkcoder69 <62224705+kkcoder69@users.noreply.github.com>
2020-11-18 14:46:59 -0500

    Removed more random code

--- .vscode/liegeJoinedMyStream.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import keyboard
-import time
-import winsound
-
-while True:
-    if pyautogui.locateOnScreen(r"images\OKLOL.png", r"images\YES.png", confidence=0.95) != None:
-        winsound.PlaySound("Ding Sound Effect.wav", winsound.SND_FILENAME)
